2575707458250d698d173f56281b3d070a67ff20
Marcel <marceq38@gmail.com>
2024-04-15 18:19:54 +0200

    yolo implement on camera

--- image_processing/image_main.py
+++ image_processing/image_main.py
@@ -1,6 +1,13 @@
 # hub.login('bf1c4516cac86a781b95844988e3da100be6c98ae5')
 
+from ultralytics import YOLO
 
+model = YOLO("model_1.pt")  # load a pretrained model (recommended for training)
+
+# Use the model
+results = model.predict(source="0",show = True)
+
+print(results)
 
 
 
