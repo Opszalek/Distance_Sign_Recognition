71d6db13ab449912092b0a39e0b22878f2f26d9b
Marcel <marceq38@gmail.com>
2024-07-15 12:45:42 +0200

    Revert "gitignore update and some learning experience xD"
    This reverts commit f4e8b47d9075d8efc9caae3d023289fbe194f59f.

---
+++ Dataset/Utils/label_extractor.py
@@ -0,0 +1,98 @@
+import os
+import pandas as pd
+import yaml
+import warnings
+
+path_to_yolo_folder = '/home/opszalek/Projekt_pikietaz/Distance_Sign_Recognition/yolov9/dataset_3'
+
+
+def load_yaml(path):
+    with open(os.path.join(path, 'data.yaml'), 'r') as file:
+        data = yaml.safe_load(file)
+    return data
+
+
+def return_class_names(data):
+    if not data['names'][-1] == 'sign':
+        raise Exception('sign is not in the list')
+    return data['names']
+
+
+def return_labels_files(path, extension):
+    try:
+        paths = os.listdir(path + '/' + extension + '/labels')
+    except FileNotFoundError:
+        warning_message = f'No such directory: {path}/{extension}/labels\n' \
+                          f'Please check the path to the labels folder.\n' \
+                          f'Or create {extension}/labels folder and put the labels there.'
+        warnings.warn(warning_message, category=UserWarning)
+        return None
+    return paths
+
+
+def check_labels(labels, extension):
+    if labels is None:
+        warning_message = (f'Labels for {extension} are not found '
+                           f'and will not be refactored.')
+        warnings.warn(warning_message, category=UserWarning)
+        return False
+    return True
+
+
+def read_data(path):
+    return pd.read_csv(path, header=None, sep=' ')
+
+
+def refactor_labels(data, class_names, sign_class_index):
+    new_data = []
+    text_labels = []
+    for index, row in data.iterrows():
+        if row[0] == sign_class_index:
+            new_data.append(row)
+        else:
+            text_labels.append(class_names[int(row[0])])
+    return new_data, text_labels
+
+
+def save_text_labels(text_labels, extension, file):
+    if not os.path.exists(os.path.join(path_to_yolo_folder, extension, 'text_labels')):
+        os.makedirs(os.path.join(path_to_yolo_folder, extension, 'text_labels'))
+    path = os.path.join(path_to_yolo_folder, extension, 'text_labels', f'{file}.txt')
+    with open(path, 'a') as file:
+        for label in text_labels:
+            file.write(label + '\n')
+
+
+def overwrite_labels(data, path):
+    data = pd.DataFrame(data)
+    data.to_csv(path, header=False, index=False, sep=' ')
+
+
+def label_manager(files, class_names, extension):
+    sign_class_index = class_names.index('sign')
+    if check_labels(files, extension):
+        for file in files:
+            path = os.path.join(path_to_yolo_folder, extension, 'labels', file)
+            data = read_data(path)
+            new_data, text_labels = refactor_labels(data, class_names, sign_class_index)
+            save_text_labels(text_labels, extension, file[:-4])
+            overwrite_labels(new_data, path)
+    else:
+        pass
+
+
+def main():
+    data = load_yaml(path_to_yolo_folder)
+    class_names = return_class_names(data)
+
+    train_labels = return_labels_files(path_to_yolo_folder, 'train')
+    valid_labels = return_labels_files(path_to_yolo_folder, 'valid')
+    test_labels = return_labels_files(path_to_yolo_folder, 'test')
+
+    label_manager(train_labels, class_names, 'train')
+    label_manager(valid_labels, class_names, 'valid')
+    label_manager(test_labels, class_names, 'test')
+
+
+if __name__ == '__main__':
+    main()
---
+++ utils/utils.py
@@ -0,0 +1,14 @@
+import time
+
+
+def timeit(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(f'{func.__name__} took {end-start} seconds to run')
+        return result
+    return wrapper
+
+# TODO: Add decorator that will measure memory usage of a function
+# Path: utils/utils.py
