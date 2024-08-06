77492541bcc029f2ebe0595aae4a74e3479c264c
Marcel <marceq38@gmail.com>
2024-08-06 22:08:27 +0200

    gitignore update and setup added

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='distance_sign_recognition',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        # List any dependencies here
+    ],
+    zip_safe=False,
+    description='Distance Sign Recognition package',
+    author='Opszalek',
+    author_email='author@example.com',
+    url='https://github.com/Opszalek/Distance_Sign_Recognition',
+)
