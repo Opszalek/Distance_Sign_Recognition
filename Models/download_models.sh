d35f481cd66ee54e34422ed6b27a13015a69ff15
Marcel <marceq38@gmail.com>
2024-09-20 19:31:04 +0200

    Added models, delete zip extraction

--- Models/download_models.sh
+++ Models/download_models.sh
@@ -15,20 +15,3 @@ mkdir -p $OUTPUT_DIR
 
 # Download the folder
 gdown --folder https://drive.google.com/drive/folders/1F-84oPjwY6zqReKcOvymW7Q_SfKByVAD -O $OUTPUT_DIR
-
-for dir in "$OUTPUT_DIR"/*/; do
-    if [ -d "$dir" ]; then
-        cd $dir
-
-        for file in *.zip; do
-            echo "Processing file: $file"
-            if [ -f "$file" ]; then
-                dirname=$(basename "$file" .zip)
-                mkdir -p "$dirname"
-                unzip -o "$file" -d "$dirname"
-                rm "$file"
-            fi
-        done
-        cd ../..
-    fi
-done
