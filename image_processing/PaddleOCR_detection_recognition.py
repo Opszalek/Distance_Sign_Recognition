8f5f8489102f1726184644986912b64ad4ea9e95
Marcel <marcelq38@gmail.com>
2024-10-10 12:42:35 +0200

    paddle settings

--- image_processing/PaddleOCR_detection_recognition.py
+++ image_processing/PaddleOCR_detection_recognition.py
@@ -2,7 +2,8 @@ from paddleocr import PaddleOCR
 
 class PaddleOCR_sign:
     def __init__(self):
-        self.ocr = PaddleOCR(use_angle_cls=True, lang='en')
+        self.ocr = PaddleOCR(use_angle_cls=False, det_db_thresh = 0.4, det_db_box_thresh = 0.5,det_db_unclip_ratio = 1.4, max_batch_size = 32,
+                det_limit_side_len = 1000, det_db_score_mode = "slow", dilation = False, lang='en', ocr_version = "PP-OCRv4")
         self.font_path = '../Models/DejaVuSans-Bold.ttf'
 
     def predict_text(self, image):
