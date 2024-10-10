4beeb7ed7fe0a71a7afca19e8661d76a4a78365f
Marcel <marcelq38@gmail.com>
2024-10-10 15:07:44 +0200

    updated downlaoding models

--- Models/download_models.sh
+++ Models/download_models.sh
@@ -14,4 +14,9 @@ OUTPUT_DIR="."
 mkdir -p $OUTPUT_DIR
 
 # Download the folder
-gdown --folder https://drive.google.com/drive/folders/1F-84oPjwY6zqReKcOvymW7Q_SfKByVAD -O $OUTPUT_DIR
+gdown --fuzzy https://drive.google.com/file/d/1qB48lRCnADnWB24bZMyeASdzFTCEAdQD/view?usp=sharing -O $OUTPUT_DIR/version_drive.txt
+
+if ! cmp -s version.txt version_drive.txt ; then
+    gdown --folder https://drive.google.com/drive/folders/1F-84oPjwY6zqReKcOvymW7Q_SfKByVAD?usp=sharing -O $OUTPUT_DIR
+    mv $OUTPUT_DIR/version_drive.txt $OUTPUT_DIR/version.txt
+fi
