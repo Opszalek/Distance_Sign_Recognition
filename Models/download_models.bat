cc0b410d8ba8ab6204748c2b6248e6078e0900ff
marcelq11 <marcelq38@gmail.com>
2024-11-11 20:59:05 +0100

    downlaod models for windows

---
+++ Models/download_models.bat
@@ -0,0 +1,25 @@
+@echo off
+REM Check if gdown is installed
+where gdown >nul 2>&1
+if %errorlevel% neq 0 (
+    echo gdown could not be found, installing...
+    pip install gdown
+)
+
+REM Set output directory
+set "OUTPUT_DIR=."
+
+REM Create output directory if it doesn't exist
+if not exist "%OUTPUT_DIR%" (
+    mkdir "%OUTPUT_DIR%"
+)
+
+REM Download version file
+gdown --fuzzy https://drive.google.com/file/d/1qB48lRCnADnWB24bZMyeASdzFTCEAdQD/view?usp=sharing -O "%OUTPUT_DIR%\version_drive.txt"
+
+REM Compare version files and download folder if they differ
+fc /b version.txt "%OUTPUT_DIR%\version_drive.txt" >nul
+if %errorlevel% neq 0 (
+    gdown --folder https://drive.google.com/drive/folders/1F-84oPjwY6zqReKcOvymW7Q_SfKByVAD?usp=sharing -O "%OUTPUT_DIR%"
+    move "%OUTPUT_DIR%\version_drive.txt" "%OUTPUT_DIR%\version.txt"
+)
